c5134a46f2288df46257cf789e0ba0097ddda74b
emre1n <emre1n@protonmail.com>
2025-07-27 15:00:10 +0300

    implement custom two-way binding using modern model function instead of input output pair

--- src/app/rect/rect.component.ts
+++ src/app/rect/rect.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, model, Output } from '@angular/core';
 
 @Component({
   selector: 'app-rect',
@@ -9,12 +9,15 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 })
 export class RectComponent {
   // Todo: Implement custom two-way binding
-  @Input({ required: true }) size!: { width: string; height: string };
-  @Output() sizeChange = new EventEmitter<{ width: string; height: string }>();
+  // @Input({ required: true }) size!: { width: string; height: string };
+  // @Output() sizeChange = new EventEmitter<{ width: string; height: string }>();
   // @NOTE: Implement custom two-way binding
   // name of the output should be exactly size + Change in order to angular to understand that this is a two-way binding
 
+  // Modern way with Angular 17.2 or newer
+  size = model.required<{ width: string; height: string }>();
+
   onReset() {
-    this.sizeChange.emit({ width: '200', height: '100' });
+    this.size.set({ width: '200', height: '100' });
   }
 }
